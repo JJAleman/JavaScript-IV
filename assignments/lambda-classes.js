e5cc38d42582fc1626782c3d38d281ec1397ed73
Juan Aleman <juan.j.aleman@gmail.com>
2019-04-25 19:12:39 -0500

    Added the base-class to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs){
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+        this.gender = attrs.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(instAttrs){
+        super(instAttrs);
+        console.log(instAttrs);
+        this.specialty = instAttrs.specialty;
+        this.favLanguage = instAttrs.favLanguage;
+        this.catchPhrase = instAttrs.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${this.subject}.`
+
+    }
+}
